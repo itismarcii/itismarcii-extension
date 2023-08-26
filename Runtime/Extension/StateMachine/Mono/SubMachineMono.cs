af1b7f3419c14e18331b6d90dc14e8ce58d002b8
itismarcii <58007324+itismarcii@users.noreply.github.com>
2023-08-26 04:47:34 +0200

    Removed abstract classification

--- Runtime/Extension/StateMachine/Mono/SubMachineMono.cs
+++ Runtime/Extension/StateMachine/Mono/SubMachineMono.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace itismarciiExtansion.Runtime.StateMachine.Mono
 {
-    public abstract class SubMachineMono
+    public class SubMachineMono
     {
         public StateMono CurrentState { get; private set; }
         
